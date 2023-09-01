129784cd3c81205e0b5056bb93de38139e56dab6
Ashish M Bhat <ashish.m.bhat98@gmail.com>
2023-09-01 15:11:45 +0530

    Initial setup

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { createRoot } from 'react-dom/client';
+import React from 'react';
+const App = () => {
+ return <h1>This is Design System</h1>;
+ }
+ const root = createRoot(document.getElementById('root'));
+root.render(<App />);
