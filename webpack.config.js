5c78e4712595287c9b2a5b98e396b52e0d0a62ea
Ashish M Bhat <ashish.m.bhat98@gmail.com>
2023-09-02 16:15:24 +0530

    Add babel.comfig, modify package.json

--- webpack.config.js
+++ webpack.config.js
@@ -6,13 +6,12 @@ const htmlPlugin = new HtmlWebPackPlugin({
 module.exports = {
 mode: 'development',
   module: {
-    rules: [{
-   test: /\.js$/,
-   exclude: /node_modules/,
-   use: {
-     loader: "babel-loader"
-   }
- },
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules(?!\/@ashish-m-bhat\/dummy-react-package)/
+      },
   {
    test: /\.css$/,
    use: ["style-loader", "css-loader"]
